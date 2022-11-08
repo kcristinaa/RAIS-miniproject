fd193468d809dea331ce8792c8a62c5c23482e0b
Sofia Yfantidou <syfantid@csd.auth.gr>
2022-11-08 17:24:42 +0200

    changes for TTM

--- preprocessing_functions.py
+++ preprocessing_functions.py
@@ -171,4 +171,15 @@ def post_preprocessing(df, isSema):
 
     return df
 
+
+def train_test_split_per_user(data, train_size=0.7):
+    users = list(set(data.id))
+    total_users = len(users)
+    slice = int(train_size*total_users)
+    users_train = users[:slice]
+    users_test = users[slice:]
+    return data[data.id.isin(users_train)], data[data.id.isin(users_test)]
+
+
+
 # --------------------------------------------------------------------------- #
