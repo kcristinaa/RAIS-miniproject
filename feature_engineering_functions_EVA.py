d7d5f23bd2706ad6f31c720cc221860753d35f8d
eparascho <eparascho@csd.auth.gr>
2022-11-29 10:57:47 +0200

    temp push EVA

--- feature_engineering_functions_EVA.py
+++ feature_engineering_functions_EVA.py
@@ -1,13 +1,12 @@
 # Creates 2 columns that represent if a user has tracked at least once its spo2 or eda
-def use_EDA_SpO2(df):
+def use_EDA_SpO2_ECG(df):
     df['spo2_tracking'] = ""
     df['EDA_tracking'] = ""
+    df['ECG_tracking'] = ""
     users = list(df['id'].unique())
 
     for user in users:
         user_df = df.loc[df['id'] == user]
-        print("NaN = ", user_df['spo2'].isnull().sum())
-        print("Lenght = ", len(user_df))
         #spo2
         if user_df['spo2'].isnull().sum() == len(user_df):
             df.loc[df['id'] == user, 'spo2_tracking'] = 0
@@ -18,6 +17,11 @@ def use_EDA_SpO2(df):
             df.loc[df['id'] == user, 'EDA_tracking'] = 0
         else:
             df.loc[df['id'] == user, 'EDA_tracking'] = 1
+        #ECG
+        if user_df['heart_rate_alert'].isnull().sum() == len(user_df):
+            df.loc[df['id'] == user, 'ECG_tracking'] = 0
+        else:
+            df.loc[df['id'] == user, 'ECG_tracking'] = 1
     return df
 
 
